9907bb770902721e06ffce5426a4162590141485
Kai <kai_tuo@flightcentre.com>
2021-10-25 17:05:16 +1000

    Changed the button to iconButton

--- src/features/todoList/NewTask.js
+++ src/features/todoList/NewTask.js
@@ -1,29 +1,30 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import { IconButton } from '@mui/material';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 export const NewTask = ({ newTask, handleChange, handleSubmit }) => {
   return (
-    <form onSubmit={handleSubmit} sx={{ display: "flex", width: 1 }}>
+    <form onSubmit={handleSubmit} sx={{ display: 'flex', width: 1 }}>
       <TextField
-        id="standard-basic"
-        label="Task"
-        variant="standard"
-        name="title"
+        id='standard-basic'
+        label='Task'
+        variant='standard'
+        name='title'
         onChange={handleChange}
-        placeholder="Title"
-        value={newTask.title || ""}
+        placeholder='Title'
+        value={newTask.title || ''}
         sx={{ width: 4 / 5 }}
         required
       />
-      <Button
-        variant="contained"
-        type="submit"
-        size="medium"
+      <IconButton
+        color='primary'
+        size='medium'
         sx={{ width: 1 / 5, mb: 4, mt: 1.5 }}
+        type='submit'
       >
-        Add
-      </Button>
+        <AddShoppingCartIcon />
+      </IconButton>
     </form>
   );
 };
