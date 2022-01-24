9cdfe8fe59811222e214cf417f57e5a8c3693d96
jacque <jacquelinezhang92@gmail.com>
2022-01-24 15:13:47 +1000

    amendment

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import { ToDoBoard } from "./features/todoList/ToDoBoard";
 import * as React from "react";
 import Grid from "@mui/material/Grid";
 
-//import pages
-import { Home } from "./page/Home";
-
 function App() {
   return (
     <Grid id="App">
